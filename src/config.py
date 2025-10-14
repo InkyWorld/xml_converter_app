724eec94fbe1bf4ea4df9244b0ffcfa52e22e802
Alex <ink404dot@gmail.com>
2025-10-14 16:14:35 +0300

    Add INPUT_DIR configuration to specify input data directory

--- src/config.py
+++ src/config.py
@@ -3,4 +3,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SCHEMA_FILE = BASE_DIR / "schemas" / "validate.xsd"
 DATA_DIR = BASE_DIR / "data"
+INPUT_DIR = BASE_DIR / "input"
 OUTPUT_DIR = BASE_DIR / "output"
